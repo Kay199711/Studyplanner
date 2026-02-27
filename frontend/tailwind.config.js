b2387801d3b003bee3c3f25adaf97ae881e00685
Mathew Valdes <mathewvaldez32@yahoo.com>
2026-02-27 01:07:55 -0500

    feat:moved sidebar code to file sidebar.jsx, added icons to tabs and made dark/light mode using global colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,8 +4,24 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+          primary:{
+            light:'#FFFFFF',
+            dark: '#0A0A0A',
+          },
+          secondary: {
+            light: '#C3D4DF',
+            dark: '#053840',
+          },
+          highlight:{
+            light: '#969696',
+            dark: '#2F2F2F',
+          },
+      },
+    },
   },
   plugins: [],
 }
