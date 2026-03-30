448eb3dc02c081c86ba2b031cd1fedae90d36781
Matthew Alvarez <mateoalva7984@gmail.com>
2026-03-29 23:38:08 -0400

    deleted duplicate files keeping the settings components with the most recent changes

--- frontend/src/components/settings/PreferencesTab.jsx
+++
@@ -1,8 +0,0 @@
-export default function PreferencesTab() {
-    return (
-      <div>
-        <h2 className="text-lg font-semibold">Preferences</h2>
-        <p className="text-sm text-icon dark:text-icon-dark">More to come</p>
-      </div>
-  );
-}
