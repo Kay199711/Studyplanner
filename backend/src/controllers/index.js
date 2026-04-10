0d869943ef8c68202368d70e9555f08d41692608
Kayla <157243147+Kay199711@users.noreply.github.com>
2026-04-10 11:30:52 -0400

    commit on main

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-export { login, logout } from './authController.js';
-=======
 export { login, register, logout } from './authController.js';
 export { updateProfile, updatePassword } from './userController.js';
 export { getNotes, createNote, updateNote, deleteNote } from './notesController.js';
 export { getShelfItems, createShelfItem, deleteShelfItem } from './shelfController.js';
->>>>>>> 0647eec6ca7e704e3552bc9037248dc00fb526a9
 export { getEvents, createEvent, updateEvent, deleteEvent } from './eventsController.js';
